59034b536577ce17125313ca59b3ce45b6b16ff7
JGLee6 <j.greendeer.lee@gmail.com>
2021-01-14 12:41:32 -0800

    Corrected x,y rectangular prism moments

--- mqlm.py
+++ mqlm.py
@@ -178,7 +178,7 @@ def rect_prism_x(L, Mx, H, a, b, phic):
                 m2k = m+2*k
                 gamsum = sp.gammaln(k+1) + sp.gammaln(m+k+1)
                 gamsum += sp.gammaln(l-m2k+2)
-                slk = (-1)**k*H**(l-m2k+1)/2**(l+m2k-1)
+                slk = (-1)**(m+k)*H**(l-m2k+1)/2**(l+m2k-1)
                 psum = 0
                 for p in range(m+k+1):
                     ksum = 0
@@ -228,7 +228,7 @@ def rect_prism_x2(L, Mx, H, a, b, phic):
     qlm : ndarray, complex
         (L+1)x(2L+1) array of complex moment values
     """
-    factor = Mx*np.sqrt(1/(4.*np.pi))
+    factor = -Mx*np.sqrt(1/(4.*np.pi))
     qlm = np.zeros([L+1, 2*L+1], dtype='complex')
     if (H <= 0) or (b <= 0) or (a <= 0):
         return qlm
@@ -243,7 +243,7 @@ def rect_prism_x2(L, Mx, H, a, b, phic):
                 m2k = m+2*k
                 gamsum = sp.gammaln(k+1) + sp.gammaln(m+k+1)
                 gamsum += sp.gammaln(l-m2k+2)
-                slk = (-1)**(m+k)*H**(l-m2k+1)/2**(l+m2k-1)
+                slk = (-1)**k*H**(l-m2k+1)/2**(l+m2k-1)
                 psum = 0
                 for p in range(m+k+1):
                     ksum = 0
@@ -309,7 +309,7 @@ def rect_prism_y(L, My, H, a, b, phic):
                 m2k = m+2*k
                 gamsum = sp.gammaln(k+1) + sp.gammaln(m+k+1)
                 gamsum += sp.gammaln(l-m2k+2)
-                slk = (-1)**k*H**(l-m2k+1)/2**(l+m2k-1)
+                slk = (-1)**(m+k)*H**(l-m2k+1)/2**(l+m2k-1)
                 psum = 0
                 for p in range(m+k+1):
                     ksum = 0
@@ -320,6 +320,72 @@ def rect_prism_y(L, My, H, a, b, phic):
                         if (jp % 2) == 1:
                             kfac = a**(m2k-jp+1)*b**(jp)
                             ksum += 1j**jp*sp.comb(k, j)*kfac/(m2k-jp+1)
+                    psum += pfac*ksum
+                slk *= psum/np.exp(gamsum)
+                qlm[l, L+m] += slk
+            # Multiply by factor dependent only on (l,m)
+            qlm[l, L+m] *= fac2
+    # Moments always satisfy q(l, -m) = (-1)^m q(l, m)*
+    ms = np.arange(-L, L+1)
+    mfac = (-1)**(np.abs(ms))
+    qlm += np.conj(np.fliplr(qlm))*mfac
+    qlm[:, L] /= 2
+    return qlm
+
+
+def rect_prism_y2(L, My, H, a, b, phic):
+    """
+    Rectangular prism centered on the origin with height H and sides of length
+    a and b extending along the x and y axes respectively when phic=0,
+    magnetized along the x-axis with a magnetization density Mx.
+
+    Inputs
+    ------
+    L : int
+        Maximum order for multipole expansion
+    My : float
+        Horizontal magnetization density of the rectangular prism
+    H : float
+        Total height of the prism
+    a : float
+        Length of prism
+    b : float
+        Width of prism
+    phic : float
+        Average angle of prism
+
+    Returns
+    -------
+    qlm : ndarray, complex
+        (L+1)x(2L+1) array of complex moment values
+    """
+    factor = -My*np.sqrt(1/(4.*np.pi))
+    qlm = np.zeros([L+1, 2*L+1], dtype='complex')
+    if (H <= 0) or (b <= 0) or (a <= 0):
+        return qlm
+    # l-m even, m odd -> l odd
+    for l in range(1, L+1, 2):
+        fac = factor*np.sqrt(2*l+1)
+        # m odd
+        for m in range(1, l+1, 2):
+            fac2 = fac*np.sqrt(np.exp(sp.gammaln(l+m+1)+sp.gammaln(l-m+1)))
+            fac2 *= np.exp(-1j*m*phic)
+            for k in range((l-m)//2+1):
+                m2k = m+2*k
+                gamsum = sp.gammaln(k+1) + sp.gammaln(m+k+1)
+                gamsum += sp.gammaln(l-m2k+2)
+                slk = (-1)**k*H**(l-m2k+1)/2**(l+m2k-1)
+                psum = 0
+                for p in range(m+k+1):
+                    ksum = 0
+                    pfac = (-1)**p*sp.comb(m+k, p)
+                    for j in range(k+1):
+                        jp = j+p
+                        if (jp % 2) == 1:
+                            kfac = (jp+1)*a**(m2k-jp+1)*b**(jp)
+                        else:
+                            kfac = (-1j)**m*(m2k-jp)*b**(m2k-jp)*a**(jp+1)
+                        ksum += 1j**(jp)*sp.comb(k, j)*kfac/((m2k+1)*(jp+1))
                     psum += pfac*ksum
                 slk *= psum/np.exp(gamsum)
                 qlm[l, L+m] += slk
